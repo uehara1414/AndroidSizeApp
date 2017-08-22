85630e9b778a6d72eac50e2b44294a5888a6b5e6
uehara <b1015062@fun.ac.jp>
2017-08-22 17:58:48 +0900

    add transition

--- app/src/main/java/com/example/uehara1414/androidsizeapp/MainActivity.java
+++ app/src/main/java/com/example/uehara1414/androidsizeapp/MainActivity.java
@@ -1,9 +1,12 @@
 package com.example.uehara1414.androidsizeapp;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Debug;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 
@@ -35,6 +38,16 @@ public class MainActivity extends AppCompatActivity {
         editSleeve.setText(sp.getString(SLEEVE, ""));
         editWaist.setText(sp.getString(WAIST, ""));
         editInserm.setText(sp.getString(INSERM, ""));
+
+        findViewById(R.id.height_button)
+                .setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        Log.d("I", "onClick");
+                        Intent intent = new Intent(MainActivity.this, HeightActivity.class);
+                        startActivity(intent);
+                    }
+                });
     }
 
     public void onSaveTapped(View view){
