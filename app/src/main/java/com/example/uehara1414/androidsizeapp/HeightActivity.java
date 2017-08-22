27aa82ce0f9b6586bc6d2764c93eb5a572223395
uehara <b1015062@fun.ac.jp>
2017-08-22 18:17:14 +0900

    implement spinner behaviour

--- app/src/main/java/com/example/uehara1414/androidsizeapp/HeightActivity.java
+++ app/src/main/java/com/example/uehara1414/androidsizeapp/HeightActivity.java
@@ -2,12 +2,40 @@ package com.example.uehara1414.androidsizeapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class HeightActivity extends AppCompatActivity {
+    private TextView myHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_height);
+
+        myHeight = (TextView)findViewById(R.id.my_height);
+
+        Spinner spinner = (Spinner)findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(
+                new AdapterView.OnItemSelectedListener() {
+                    @Override
+                    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                        Spinner spinner = (Spinner)adapterView;
+                        String item = spinner.getSelectedItem().toString();
+                        if(!item.isEmpty()){
+                            myHeight.setText(item);
+                        }
+                    }
+
+                    @Override
+                    public void onNothingSelected(AdapterView<?> adapterView) {
+
+                    }
+                }
+        );
     }
 }
