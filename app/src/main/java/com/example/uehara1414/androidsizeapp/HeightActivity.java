ca3a9836a55f214cb04abea339f65d36ec0f7f24
uehara <b1015062@fun.ac.jp>
2017-08-22 17:49:16 +0900

    setup widgets constraint

---
+++ app/src/main/java/com/example/uehara1414/androidsizeapp/HeightActivity.java
@@ -0,0 +1,13 @@
+package com.example.uehara1414.androidsizeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeightActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_height);
+    }
+}
